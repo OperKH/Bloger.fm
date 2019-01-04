dbfa50c516d013be0cbc0d5879a9b85cb3553529
OperKH <oper.kh@gmail.com>
2019-01-04 12:46:31 +0200

    Add info links. Try to open Facebook app first.

---
+++ lib/models/info_link.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class InfoLink {
+  final String text;
+  final String url;
+  final IconData icon;
+
+  const InfoLink(this.text, this.url, this.icon);
+}
--- lib/pages/player.dart
+++ lib/pages/player.dart
@@ -2,26 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../models/radiostation.dart';
+import '../models/info_link.dart';
 import '../constants/radiostations.dart';
 import '../providers/radiostations_provider.dart';
 import '../widgets/platform/platform_progress_indicator.dart';
 
 const String facebookUrl = 'https://facebook.com/bloger.fm';
+const String facebookAndroid = 'fb://page/885939701490818?referrer=app_link';
+const String facebookIOS = 'fb://page/?id=885939701490818';
 const String twitterUrl = 'https://twitter.com/BlogerFm';
 const String gPlusUrl = 'https://plus.google.com/+BlogerFM';
 const String soundCloudUrl = 'https://soundcloud.com/blogerfm';
 
+const List<InfoLink> infoLinks = const [
+  InfoLink('Про нас', 'http://bloger.fm/o-nas.html', Icons.person),
+  InfoLink('Програми', 'http://bloger.fm/programmy.html', Icons.schedule),
+  InfoLink('(067) 21-21-000', 'tel://+380672121000', Icons.phone),
+  InfoLink(
+      'info@bloger.fm', 'mailto:info@bloger.fm', FontAwesomeIcons.envelope),
+  InfoLink('bloger.fm', 'skype:bloger.fm?chat', FontAwesomeIcons.skype),
+  InfoLink(
+      '04213 Київ, вул. Прирічна, 27-е',
+      'https://www.google.com.ua/maps/@50.5239043,30.5191951,17z/data=!4m5!3m4!1s0x40d4d230eb53e497:0x82c45390281964f1!8m2!3d50.5239043!4d30.5213838',
+      Icons.location_on),
+];
+
 class PlayerPage extends StatelessWidget {
   Widget _buildDrawer(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: <Widget>[
-          AppBar(
-            title: Text('Bloger.FM radio'),
-            automaticallyImplyLeading: false,
-          ),
-          _buildRadiostationsList(context),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              title: Text('Bloger.FM radio'),
+              automaticallyImplyLeading: false,
+            ),
+            _buildRadiostationsList(context),
+            SizedBox(height: 16.0),
+            _buildInfoLinks(context),
+          ],
+        ),
       ),
     );
   }
@@ -47,6 +67,24 @@ class PlayerPage extends StatelessWidget {
           );
         }).toList());
       },
+    );
+  }
+
+  Widget _buildInfoLinks(BuildContext context) {
+    return Column(
+      children: infoLinks.map((InfoLink infoLink) {
+        return ListTile(
+          dense: true,
+          leading: Icon(infoLink.icon),
+          title: Text(infoLink.text),
+          onTap: () async {
+            if (await canLaunch(infoLink.url)) {
+              await launch(infoLink.url);
+            }
+            Navigator.of(context).pop();
+          },
+        );
+      }).toList(),
     );
   }
 
@@ -165,8 +203,15 @@ class PlayerPage extends StatelessWidget {
             color: Color(0xFF365899),
           ),
           onPressed: () async {
+            final intentLink = Theme.of(context).platform == TargetPlatform.iOS
+                ? facebookIOS
+                : facebookAndroid;
             try {
-              await launch(facebookUrl);
+              if (await canLaunch(intentLink)) {
+                await launch(intentLink);
+              } else {
+                await launch(facebookUrl);
+              }
             } catch (e) {
               print(e);
             }
@@ -199,6 +244,7 @@ class PlayerPage extends StatelessWidget {
           },
         ),
         IconButton(
+          alignment: Alignment.centerLeft,
           icon: Icon(
             FontAwesomeIcons.soundcloud,
             color: Color(0xFFFF5500),
